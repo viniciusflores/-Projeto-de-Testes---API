774dde779e1e5752baa3298049b3a23b42460550
vinicius flores <viniciusflores379@gmail.com>
2019-07-16 21:46:48 -0300

    :sparkles: add second test

--- src/main/java/test/MarvelTest.java
+++ src/main/java/test/MarvelTest.java
@@ -19,4 +19,24 @@ public class MarvelTest extends BaseTest {
 			.body("status", is("You may not request more than 100 items."))
 		;
 	}
+	
+	@Test
+	public void mustValidateNumberOfWolveriniMagazines() {
+		int idWolverine = given()
+			.queryParam("name", "Wolverine")
+		.when()
+			.get("/characters")
+		.then()
+			.statusCode(200)
+			.extract().path("data.results[0].id")
+		;
+		
+		given()
+		.when()
+			.get("/characters/"+idWolverine+"/comics")
+		.then()
+			.statusCode(200)
+			.body("data.total", is(2348))
+		;
+	}
 }
